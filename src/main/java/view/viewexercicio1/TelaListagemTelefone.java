f0a0f38468ee148adb4ec3448474a83aa44bb864
arthu <arthu@DESKTOP-5EJD8MN>
2020-04-03 18:43:30 -0300

    Tabela de telefones atualizada, foram adicionadas duas colunas.

--- src/main/java/view/viewexercicio1/TelaListagemTelefone.java
+++ src/main/java/view/viewexercicio1/TelaListagemTelefone.java
@@ -14,14 +14,13 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import controller.exercicio1.TelefoneController;
-import model.dao.exercicio1.ClienteDAO;
 import model.vo.exercicio1.Telefone;
 
 public class TelaListagemTelefone extends JFrame {
 
 	private JPanel contentPane;
 	private ArrayList<Telefone> telefones;
-	private String[] nomeColunas = { "Código País", "DDD", "Numero", "Dono" };
+	private String[] nomeColunas = { "Código País", "DDD", "Numero", "Ativo", "Movel", "Dono" };
 	private JTable tblTelefones;
 
 	private void limparTabela() {
@@ -33,20 +32,31 @@ public class TelaListagemTelefone extends JFrame {
 		DefaultTableModel model = (DefaultTableModel) tblTelefones.getModel();
 		for (Telefone telefone : telefones) {
 			String teste = "teste";
-			Object[] novaLinha = new Object[4];
-			
+			Object[] novaLinha = new Object[6];
+
 			novaLinha[0] = telefone.getCodigoPais();
 			novaLinha[1] = telefone.getDdd();
 			novaLinha[2] = telefone.getNumero();
+			if (telefone.isAtivo()) {
+				novaLinha[3] = true;
+			} else {
+				novaLinha[3] = false;
+			}
+			if (telefone.isMovel()) {
+				novaLinha[4] = true;
+			} else {
+				novaLinha[4] = false;
+			}
+			if (telefone.getDono().getId() == 0) {
+				novaLinha[5] = "Não possui";
+			} else {
+				novaLinha[5] = telefone.getDono().getNome();
 
-				if (telefone.getDono().getId() == 0) {
-					novaLinha[3] = "Nenhum";
-				}
-				novaLinha[3] = telefone.getDono().getNome();
-
+			}
+			
 			model.addRow(novaLinha);
 		}
-	
+
 	}
 
 	/**
@@ -72,21 +82,21 @@ public class TelaListagemTelefone extends JFrame {
 	 */
 	public TelaListagemTelefone() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 507, 333);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JPanel panel = new JPanel();
-		panel.setBounds(12, 68, 408, 133);
+		panel.setBounds(0, 54, 489, 125);
 		contentPane.add(panel);
 
 		tblTelefones = new JTable();
 		panel.add(tblTelefones);
 
 		JLabel lblListagem = new JLabel("Listagem de Telefones");
-		lblListagem.setBounds(143, 25, 129, 16);
+		lblListagem.setBounds(183, 25, 129, 16);
 		contentPane.add(lblListagem);
 
 		JButton btnListar = new JButton("Listar");
@@ -97,7 +107,7 @@ public class TelaListagemTelefone extends JFrame {
 				atualizarTabela();
 			}
 		});
-		btnListar.setBounds(175, 214, 97, 25);
+		btnListar.setBounds(196, 248, 97, 25);
 		contentPane.add(btnListar);
 	}
 }
